1dc39a8e4be863abf98d827b9c194aa005f63ef6
SUMEET <SUMEET>
2017-02-25 15:40:08 +0530

    Local version to run on systems. All files added...

---
+++ WebContent/js/user/FactsController.js
@@ -0,0 +1,109 @@
+fact.controller("factController", function($scope,$route, $http,$routeParams) {
+	
+		$scope.factForm = {};
+		$scope.factForm1 = {};
+		$scope.factForm.list = null;
+		$scope.factForm.category = null;
+		$scope.factForm.pieData=null;
+		$scope.factForm1.list = null;
+		$scope.factForm1.Newlist = null;
+		$scope.factForm1.Randomlist = null;
+		$scope.factForm1.category = null;
+		
+		$scope.init = function(){	
+			
+			$scope.factForm1.getFacts(); 
+			$scope.factForm.getPieData();
+		};
+		
+		 
+		 $scope.$on('$routeChangeSuccess', function(event,next, current) { 
+			   //... you could trigger something here ...
+			 //console.log("ghh");
+			 
+			 var str = next.params.value;
+			 if (str=="BIOLOGY")
+			    {
+			    document.body.style.background="#ff4d88";
+			   	}
+		     else if (str=="CHEMISTRY")
+			    {
+			    document.body.style.background="#1aff8c";
+			   	}
+			 else if (str=="MATHS")
+				{
+				document.body.style.background="#884dff";
+				}
+			 else if (str=="PHYSICS")
+				{
+				document.body.style.background="#00cccc";
+				}
+			 else if (str=="SCIENTIST")
+				{
+				document.body.style.background="#ffb84d";
+				}
+			 else if (str=="WWII")
+				{
+				document.body.style.background="#ff944d";
+				}
+			 else if (str=="Home")
+				{
+				document.body.style.background="#009999";
+				}
+
+			 });
+		
+		
+	$scope.factForm.getCategoryList = function() {
+		
+				
+		var responsePromise = $http.get(URI + "FactAPI" + "/distinctCategory");
+
+		responsePromise.then(function(response) {
+			$scope.factForm.list = response.data;
+			// console.log($scope.factForm.list);
+		}, function(response) {
+			$scope.factForm.list = null;
+		});
+
+	};
+	
+	
+	$scope.factForm.getPieData=function(){
+		//alert("ss")
+		var responsePromise=$http.get(URI+"FactAPI"+"/countFacts");
+		responsePromise.then(function(response){
+			$scope.factForm.pieData=response.data;
+		},function(response){
+			$scope.factForm.pieData=null;
+		});
+		
+	};
+	
+    
+	$scope.factForm1.getFacts = function() {
+		$scope.factForm1.category=$routeParams.value;
+		$scope.factForm.category=$routeParams.value;
+		//alert($scope.factForm.category);		
+		
+		var responsePromise = $http.get(URI + "FactAPI" + "/category/"+$routeParams.value);
+		responsePromise.then(function(response) {
+			$scope.factForm1.list = response.data;
+			
+			
+			$scope.factForm1.Newlist=response.data;
+			s=$scope.factForm1.Newlist.length;
+			
+			 var x = Math.floor((Math.random() *(s-1)) + 0);
+			//alert(x);
+			$scope.factForm1.Randomlist=$scope.factForm1.Newlist[x];
+				
+		}, function(response) {
+			$scope.factForm1.list = null;
+
+		});
+	};
+	
+	$scope.init();
+
+});
---
+++ src/com/smith/facts/business/service/FactServiceImpl.java
@@ -0,0 +1,38 @@
+package com.smith.facts.business.service;
+
+
+import java.util.List;
+
+import com.smith.facts.bean.Facts;
+import com.smith.facts.dao.FactDAO;
+import com.smith.facts.resources.Factory;
+
+public class FactServiceImpl implements FactService {
+
+	public List<Facts> getFacts(Integer factId) throws Exception {
+		// TODO Auto-generated method stub
+		FactDAO dao=Factory.createFactDAO();
+		List<Facts> result=dao.getFact(factId);
+		return result;
+	}
+
+
+	public List<Facts> getFactsCategory(String category) throws Exception {
+		FactDAO dao=Factory.createFactDAO();
+		List<Facts> result=dao.getFactsCategory(category);
+		return result;
+	}
+	
+	public List<String> getDistinctcatergories() throws Exception{
+		FactDAO dao=Factory.createFactDAO();
+		List<String> result=dao.getDistinctcatergories();
+		return result;
+	}
+
+	public List<Integer> getCountCategories() throws Exception {
+		FactDAO dao=Factory.createFactDAO();
+		List<Integer> result=dao.getCountCategories();
+		return result;
+	}
+	
+}
